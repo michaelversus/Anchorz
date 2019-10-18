effea2d8535196f2f861fe99f2be7dc7f5753f9e
Michalis Karagiorgos <mk@mikdev.eu>
2019-10-18 21:19:13 +0300

    Initial project setup

---
+++ Example/Pods/Target Support Files/Anchorz/Anchorz-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Anchorz : NSObject
+@end
+@implementation PodsDummy_Anchorz
+@end
---
+++ Example/Pods/Target Support Files/Pods-Anchorz_Example/Pods-Anchorz_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Anchorz_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Anchorz_Example
+@end
